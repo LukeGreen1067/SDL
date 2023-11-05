e46973ef794f44618f823773932f43b29472d6b1
Luke Green CS2019 <luke.green.2019@uni.strath.ac.uk>
2023-11-05 01:58:58 +0000

    Realised that my code is kinda ass gonna refactor this was due to a lack of understanding

--- Code/raytrace_raycast.cpp
+++ Code/raytrace_raycast.cpp
@@ -10,10 +10,13 @@
 // http://crait.net/download.php?file=CircuitDude.ino
 // Cool game that I will be basing code off of to make my engine
 
+//NEED TO REFACTOR CODE TOMMOROW GONNA CRY :(
+
 // Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <vector>
 
 // map constants
 #define MAPHEIGHT 10
@@ -28,6 +31,12 @@
 #define STATE_QUIT 3
 
 unsigned char gameState = STATE_MENU;
+double posX = 22, posY = 12;
+double dirX = -1, dirY = 0;
+double planeX = 0, planeY = 0.66;
+
+double time = 0;
+double oldTime = 0;
 
 const uint8_t *keystate = SDL_GetKeyboardState(NULL);
 
@@ -48,32 +57,93 @@ void getInput()
 {
 	// Work out how to make SDL buttons for the game start even if this is just a menu system
 	// Will have to split this code to make the game inputs and menu inputs
-	if (keystate[SDL_SCANCODE_A])
-	{
-		gameState = STATE_MENU;
-	}
-	if (keystate[SDL_SCANCODE_D])
-	{
-		gameState = STATE_PLAY;
-	}
-
-	if (keystate[SDL_SCANCODE_W])
-	{
-		gameState = STATE_ABOUT;
-	}
-
-	if (keystate[SDL_SCANCODE_S])
-	{
-		gameState = STATE_QUIT;
-	}
+	if (keystate[SDL_SCANCODE_W]){}
+	if (keystate[SDL_SCANCODE_A]){}
+	if (keystate[SDL_SCANCODE_S]){}
+	if (keystate[SDL_SCANCODE_D]){}
+	if (keystate[SDL_SCANCODE_UP]){gameState = STATE_MENU;}
+	if (keystate[SDL_SCANCODE_RIGHT]){gameState = STATE_PLAY;}
+	if (keystate[SDL_SCANCODE_LEFT]){gameState = STATE_ABOUT;}
+	if (keystate[SDL_SCANCODE_DOWN]){gameState = STATE_QUIT;}
 };
+
+void renderScreen()
+{
+	for (int x = 0; x < SCREEN_WIDTH; x++)
+	{
+		double cameraX = 2 * x / double(SCREEN_WIDTH) - 1; // x-coordinate in camera space
+		double rayDirX = dirX + planeX * cameraX;
+		double rayDirY = dirY + planeY * cameraX;
+		int mapX = int(posX);
+		int mapY = int(posY);
+
+		double sideDistX, sideDistY;
+
+		double deltaDistX = (rayDirX == 0) ? 1e30 : std::abs(1 / rayDirX);
+		double deltaDistY = (rayDirY == 0) ? 1e30 : std::abs(1 / rayDirY);
+		double perpWallDist;
+
+		int stepX;
+		int stepY;
+
+		int hit = 0; // was there a wall hit?
+		int side;	 // was a NS or a EW wall hit?
+		if (rayDirX < 0){
+			stepX = -1;
+			sideDistX = (posX - mapX) * deltaDistX;
+		}
+		else{
+			stepX = 1;
+			sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+		}
+		if (rayDirY < 0){
+			stepY = -1;
+			sideDistY = (posY - mapY) * deltaDistY;
+		}
+		else{
+			stepY = 1;
+			sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+		}
+
+		while (hit == 0)
+		{
+			if (sideDistX < sideDistY){
+				sideDistX += deltaDistX;
+				mapX += stepX;
+				side = 0;
+			}
+			else{
+				sideDistY += deltaDistY;
+				mapY += stepY;
+				side = 1;
+			}
+			// Check if ray has hit a wall
+			if (map[mapX][mapY] > 0){hit = 1;}
+
+			if (side == 0){perpWallDist = (sideDistX - deltaDistX);}
+			else{perpWallDist = (sideDistY - deltaDistY);}
+
+			// Calculate height of line to draw on screen
+			int lineHeight = (int)(SCREEN_HEIGHT / perpWallDist);
+
+			// calculate lowest and highest pixel to fill in current stripe
+			int drawStart = -lineHeight / 2 + SCREEN_HEIGHT / 2;
+			int drawEnd = lineHeight / 2 + SCREEN_HEIGHT / 2;
+			if (drawStart < 0){drawStart = 0;}
+			if (drawEnd >= SCREEN_HEIGHT){drawEnd = SCREEN_HEIGHT - 1;}
+
+
+		}
+	}
+}
 
 void getMenuState(SDL_Surface *screenSurface)
 {
 	switch (gameState)
 	{
 	case STATE_MENU:
-		SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0x00)); // Set colour Red
+		SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0x00)); // Set colour Red#
+		// TODO: Default value should be a load menu screen
 		break;
 	case STATE_PLAY:
 		SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0xFF, 0x00)); // Set colour Green
@@ -81,6 +151,7 @@ void getMenuState(SDL_Surface *screenSurface)
 		break;
 	case STATE_ABOUT:
 		SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0xFF)); // Set colour Blue
+		// TODO: Render text to the screen watch this be the worst bit
 		break;
 	case STATE_QUIT:
 		break;
@@ -92,6 +163,7 @@ int main(int argc, char *args[])
 
 	SDL_Window *window = NULL;		   // The window we'll be rendering to
 	SDL_Surface *screenSurface = NULL; // The surface contained by the window
+	SDL_Renderer *renderer = NULL;
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
@@ -115,6 +187,8 @@ int main(int argc, char *args[])
 			if (e.type == SDL_QUIT)
 				quit = true;
 		}
+		
+
 
 		getInput();
 		getMenuState(screenSurface);
